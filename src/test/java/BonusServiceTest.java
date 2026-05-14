da5f5c6e2eeb67a42a0b80c1a920826f54ade026
Andrey Kolosov <Kolad1970@gmail.com>
2026-05-14 14:12:15 +0600

    Система сборки Maven, управление зависимостями, авто-тесты на JUnit5/ Ответ получен, но не понятен смысл сказанного 2

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -10,7 +10,7 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 50;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -41,7 +41,7 @@ public class BonusServiceTest {
             // подготавливаем данные:
             long amount = 1000;
             boolean registered = false;
-            long expected = 10;
+            long expected = 20;
 
             // вызываем целевой метод:
             long actual = service.calculate(amount, registered);
